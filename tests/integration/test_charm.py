f1eac1286dad9983834309982f2fff4c48102382
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-04-12 15:06:00 +0300

    update constants

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -17,10 +17,8 @@ METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = "metacontroller-operator"
 PROMETHEUS = "prometheus-k8s"
 PROMETHEUS_CHANNEL = "latest/stable"
-PROMETHEUS_TRUST = True
 GRAFANA = "grafana-k8s"
 GRAFANA_CHANNEL = "latest/stable"
-GRAFANA_TRUST = True
 PROMETHEUS_SCRAPE = "prometheus-scrape-config-k8s"
 PROMETHEUS_SCRAPE_CHANNEL = "latest/stable"
 
