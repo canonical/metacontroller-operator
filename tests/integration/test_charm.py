44e35982fce4d2fa07f1f69d07ce384579919ec9
Michal Hucko <michal.hucko@canonical.com>
2024-10-11 12:53:21 +0200

    Add alert rules to metacontroller-operator based on the KF093 spec (#124) (#133)
    * Add alert rules to metacontroller-operator based on the KF093 spec
    
    * Delete src/prometheus_alert_rules/unit_unavailable.rule
    
    * add cos integration tests for alert rules
    
    Co-authored-by: Robert Gildein <gildeinrobert@gmail.com>

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -1,26 +1,23 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import json
 import logging
 from pathlib import Path
 
 import pytest
-import requests
-import tenacity
 import yaml
+from charmed_kubeflow_chisme.testing import (
+    assert_alert_rules,
+    assert_metrics_endpoint,
+    deploy_and_assert_grafana_agent,
+    get_alert_rules,
+)
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = "metacontroller-operator"
-PROMETHEUS = "prometheus-k8s"
-PROMETHEUS_CHANNEL = "1.0/stable"
-GRAFANA = "grafana-k8s"
-GRAFANA_CHANNEL = "1.0/stable"
-PROMETHEUS_SCRAPE = "prometheus-scrape-config-k8s"
-PROMETHEUS_SCRAPE_CHANNEL = "1.0/stable"
 
 
 @pytest.mark.abort_on_fail
@@ -54,82 +51,28 @@ async def test_build_and_deploy_with_trust(ops_test: OpsTest):
             ), f"Application {app_name}.Unit {i_unit}.workload_status != active"
     assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
 
-
-async def test_prometheus_grafana_integration(ops_test: OpsTest):
-    """Deploy prometheus, grafana and required relations, then test the metrics."""
-    scrape_config = {"scrape_interval": "30s"}
-
-    await ops_test.juju(
-        "deploy",
-        PROMETHEUS,
-        "--channel",
-        PROMETHEUS_CHANNEL,
-        "--trust",
-        check=True,
-    )
-    await ops_test.juju(
-        "deploy",
-        GRAFANA,
-        "--channel",
-        GRAFANA_CHANNEL,
-        "--trust",
-        check=True,
-    )
-    await ops_test.model.deploy(
-        PROMETHEUS_SCRAPE,
-        channel=PROMETHEUS_SCRAPE_CHANNEL,
-        config=scrape_config,
+    # Deploying grafana-agent-k8s and add all relations
+    await deploy_and_assert_grafana_agent(
+        ops_test.model, APP_NAME, metrics=True, dashboard=True, logging=False
     )
 
-    await ops_test.model.add_relation(APP_NAME, PROMETHEUS_SCRAPE)
-    await ops_test.model.add_relation(
-        f"{PROMETHEUS}:grafana-dashboard", f"{GRAFANA}:grafana-dashboard"
-    )
-    await ops_test.model.add_relation(
-        f"{APP_NAME}:grafana-dashboard", f"{GRAFANA}:grafana-dashboard"
-    )
-    await ops_test.model.add_relation(
-        f"{PROMETHEUS}:metrics-endpoint",
-        f"{PROMETHEUS_SCRAPE}:metrics-endpoint",
-    )
 
-    await ops_test.model.wait_for_idle(status="active", timeout=60 * 20)
-
-    status = await ops_test.model.get_status()
-    prometheus_unit_ip = status["applications"][PROMETHEUS]["units"][f"{PROMETHEUS}/0"]["address"]
-    logger.info(f"Prometheus available at http://{prometheus_unit_ip}:9090")
-
-    for attempt in retry_for_5_attempts:
-        logger.info(
-            f"Testing prometheus deployment (attempt " f"{attempt.retry_state.attempt_number})"
-        )
-        with attempt:
-            r = requests.get(
-                f"http://{prometheus_unit_ip}:9090/api/v1/query?"
-                f'query=up{{juju_application="{APP_NAME}"}}'
-            )
-            response = json.loads(r.content.decode("utf-8"))
-            response_status = response["status"]
-            logger.info(f"Response status is {response_status}")
-            assert response_status == "success"
-
-            response_metric = response["data"]["result"][0]["metric"]
-            assert response_metric["juju_application"] == APP_NAME
-            assert response_metric["juju_model"] == ops_test.model_name
-
-            # Assert the unit is available by checking the query result
-            # The data is presented as a list [1707357912.349, '1'], where the
-            # first value is a timestamp and the second value is the state of the unit
-            # 1 means available, 0 means unavailable
-            assert response["data"]["result"][0]["value"][1] == "1"
+async def test_metrics_enpoint(ops_test):
+    """Test metrics_endpoints are defined in relation data bag and their accessibility.
+    This function gets all the metrics_endpoints from the relation data bag, checks if
+    they are available from the grafana-agent-k8s charm and finally compares them with the
+    ones provided to the function.
+    """
+    app = ops_test.model.applications[APP_NAME]
+    await assert_metrics_endpoint(app, metrics_port=9999, metrics_path="/metrics")
 
 
-# Helper to retry calling a function over 30 seconds or 5 attempts
-retry_for_5_attempts = tenacity.Retrying(
-    stop=(tenacity.stop_after_attempt(5) | tenacity.stop_after_delay(30)),
-    wait=tenacity.wait_exponential(multiplier=1, min=1, max=10),
-    reraise=True,
-)
+async def test_alert_rules(ops_test):
+    """Test check charm alert rules and rules defined in relation data bag."""
+    app = ops_test.model.applications[APP_NAME]
+    alert_rules = get_alert_rules()
+    logger.info("found alert_rules: %s", alert_rules)
+    await assert_alert_rules(app, alert_rules)
 
 
 # TODO: Add test for charm removal
